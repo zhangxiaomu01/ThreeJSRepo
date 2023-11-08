6b09fb46b3e855cb150780c60785ac90b74365b9
Jun Zhang <zhangxiaomu01@gmail.com>
2023-11-08 13:57:24 +0800

    Adds the jsdoc related logic to the project.

--- src/GLTFLoaderTest.js
+++ src/GLTFLoaderTest.js
@@ -1,10 +1,23 @@
 import * as THREE from 'three';
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js'
 
+/**
+ * GLTFLoaderTest class.
+ */
 class GLTFLoaderTest {
+    /**
+     * A default constructor.
+     * @constructor
+     */
     constructor() {
         console.log("GenerateTestData executed!");
     }
+
+    /**
+     * Loads the current GLTF model.
+     * 
+     * @param {THREE.scene} currentScene the current Three scene. 
+     */
     static LoadGLTFModel(currentScene) {
         const loader = new GLTFLoader();
         loader.load('./threejs_r155/examples/models/gltf/RobotExpressive/RobotExpressive.glb', function (model) {
