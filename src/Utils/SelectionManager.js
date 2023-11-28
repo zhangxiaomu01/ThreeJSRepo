f2539e91ef0225bab1b49c5cd049547accee492c
Jun Zhang <zhangxiaomu01@gmail.com>
2023-11-28 17:52:23 +0800

    Adds a new SelectionManager class to the project.

---
+++ src/Utils/SelectionManager.js
@@ -0,0 +1,35 @@
+import * as THREE from 'three';
+
+/**
+ * SelectionManager class.
+ */
+class SelectionManager {
+    /**
+     * A default constructor.
+     * @constructor
+     */
+    constructor() {
+        console.log("SelectionManager created!");
+    }
+
+    static onPointerDown( event, scene, camera, callback ) {
+        const raycaster = new THREE.Raycaster();
+        const mouse = new THREE.Vector2();
+
+        mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
+        mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+
+        raycaster.setFromCamera( mouse, camera );
+        const intersects = raycaster.intersectObjects( scene.children, false );
+        if ( intersects.length > 0 ) {
+
+            const object = intersects[ 0 ].object;
+            callback(object);
+            render();
+        }
+
+    }
+
+};
+
+export {SelectionManager}
