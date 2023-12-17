e8f75e4e27d447e4605b30ecf39318da5dc2966c
Jun Zhang <31782250+zhangxiaomu01@users.noreply.github.com>
2023-12-17 17:40:47 +0800

    Adds the extra installs to the project.
    1. Adds the webpack-bundle-analyzer
    2. Adds the webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     entry: "./src/main.js",
@@ -7,4 +8,7 @@ module.exports = {
         filename: "main.js",
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new BundleAnalyzerPlugin(),
+    ],
 }
