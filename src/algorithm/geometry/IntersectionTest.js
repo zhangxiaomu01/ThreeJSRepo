ee86f3a1ea55b48d5e0818d65e36ef31aa349d56
Jun Zhang <31782250+zhangxiaomu01@users.noreply.github.com>
2024-03-17 22:51:55 +0800

    Add the triangle intersection algorithm

--- src/algorithm/geometry/IntersectionTest.js
+++ src/algorithm/geometry/IntersectionTest.js
@@ -1,9 +1,10 @@
 // Stat.js
+import { parseInt } from 'lodash';
 import Stats from '../../../threejs_r155/examples/jsm/libs/stats.module.js';
 import { THREE, OrbitControls, GUI, mergeGeometries } from '../../CommonImports.js'
 
 
-const gridSize = 1;
+const gridSize = 2;
 const gridNumber = 32;
 const offSet = gridSize / 2;
 
@@ -49,9 +50,21 @@ class IntersectionTest {
         this.scene.add(directionalLight);
         // this.scene.add(directionalLightHelper);
 
-        this.camera = new THREE.PerspectiveCamera(30, width / height, 1, 3000);
+        // this.camera = new THREE.PerspectiveCamera(30, width / height, 1, 3000);
+        var frustumSize = 1000; //设置显示相机前方1000高的内容
+        var aspect = window.innerWidth / window.innerHeight; //计算场景的宽高比
+        const aspectRatio = 1.0
+        this.camera = new THREE.OrthographicCamera( 
+            frustumSize * aspect / - aspectRatio, 
+            frustumSize * aspect / aspectRatio,
+            frustumSize / aspectRatio, 
+            frustumSize / - aspectRatio, 
+            1,
+            2000 
+        );
+
+        // this.camera = new THREE.OrthographicCamera();
         this.camera.position.set(80, 80, 80);
-        this.camera.lookAt(this.mesh.position);
 
         this.renderer = new THREE.WebGLRenderer( {antialias: true,} );
         this.renderer.setSize(width, height);
@@ -104,19 +117,185 @@ class IntersectionTest {
         this.renderer.render(this.scene, this.camera); //Render
     }
 
+    isSame(x, y) {
+        return Math.abs(x - y) < 1e-10;
+    }
+
+    findPointOnLine(x1, y1, x2, y2, givenCoordinate) {
+        if (this.isSame(x1, x2) || this.isSame(y1, y2)) {
+            return undefined;
+        }
+        // Calculate slope of the line
+        let m = (y2 - y1) / (x2 - x1);
+        
+        // Calculate y-intercept of the line
+        let b = y1 - m * x1;
+
+        let minX = Math.min(x1, x2);
+        let maxX = Math.max(x1, x2);
+        let minY = Math.min(y1, y2);
+        let maxY = Math.max(y1, y2);
+        
+        // Determine if the given coordinate is x or y based on the input
+        if (typeof givenCoordinate.x !== 'undefined') {
+            // Given x, solve for y
+            let y = m * givenCoordinate.x + b;
+
+            if (y < minY || y > maxY) return undefined;
+
+            return { x: givenCoordinate.x, y };
+        } else if (typeof givenCoordinate.y !== 'undefined') {
+            // Given y, solve for x
+            // Handle the special case where the line is vertical
+            if (x2 === x1) {
+                console.warn("The line is vertical; the x-coordinate will be the same as the given points.");
+                return { x: x1, y: givenCoordinate.y };
+            } else {
+                let x = (givenCoordinate.y - b) / m;
+
+                if (x < minX || x > maxX) return undefined;
+
+                return { x, y: givenCoordinate.y };
+            }
+        } else {
+            throw new Error("Invalid input: Please provide either an x or y coordinate.");
+        }
+    }
+
+    drawLine(grid, x0, y0, x1, y1, gridSize) {
+        let dx = Math.abs(x1 - x0);
+        let dy = -Math.abs(y1 - y0);
+        let sx = x0 < x1 ? gridSize : -gridSize;
+        let sy = y0 < y1 ? gridSize : -gridSize;
+        let err = dx + dy;
+        let e2;
+    
+        while (true) {
+            if (x0 >= 0 && x0 < grid.length * gridSize && y0 >= 0 && y0 < grid[0].length * gridSize) {
+                let indexX = parseInt(x0 / gridSize);
+                let indexY = parseInt(y0 / gridSize);
+                grid[indexX][indexY] = 1; // Mark the voxel as part of the line
+
+                // 检查相交的grid的四条边
+                // Need to check the best combo
+                let gX0 = (indexX - 1) * gridSize;
+                let gX1 = (indexX + 1) * gridSize;
+                let gY0 = (indexY - 1) * gridSize;
+                let gY1 = (indexY + 1) * gridSize;
+
+                let res = this.findPointOnLine(x0, y0, x1, y1, { x: gX0});
+                if (res !== undefined && res.y > 0) {
+                    let indexX1 = parseInt(res.x / gridSize);
+                    let indexY1 = parseInt(res.y / gridSize);
+                    if (indexX1 < grid.length && indexY1 < grid[0].length) {
+                        grid[indexX1][indexY1] = 2;
+                        grid[indexX1 - 1][indexY1] = 2;
+                    }
+                }
+
+                res = this.findPointOnLine(x0, y0, x1, y1, { x: gX1});
+                if (res !== undefined && res.y > 0) {
+                    let indexX1 = parseInt(res.x / gridSize);
+                    let indexY1 = parseInt(res.y / gridSize);
+                    if (indexX1 < grid.length && indexY1 < grid[0].length)
+                        grid[indexX1][indexY1] = 2;
+                        // grid[indexX1 - 1][indexY1] = 2;
+                }
+
+                res = this.findPointOnLine(x0, y0, x1, y1, { y: gY0});
+                if (res !== undefined && res.x > 0) {
+                    let indexX1 = parseInt(res.x / gridSize);
+                    let indexY1 = parseInt(res.y / gridSize);
+                    if (indexX1 < grid.length && indexY1 < grid[0].length)
+                        grid[indexX1][indexY1] = 2;
+                        grid[indexX1][indexY1 - 1] = 2;
+                }
+
+                res = this.findPointOnLine(x0, y0, x1, y1, { y: gY1});
+                if (res !== undefined && res.x > 0) {
+                    let indexX1 = parseInt(res.x / gridSize);
+                    let indexY1 = parseInt(res.y / gridSize);
+                    if (indexX1 < grid.length && indexY1 < grid[0].length)
+                        grid[indexX1][indexY1] = 2;
+                }
+
+            } else {
+                break;
+            }
+    
+            // if (x0 >= grid.length * gridSize || y0 >= grid[0].length * gridSize) break;
+
+            e2 = 2 * err;
+            if (e2 >= dy) {
+                err += dy;
+                x0 += sx;
+            }
+            if (e2 <= dx) {
+                err += dx;
+                y0 += sy;
+            }
+        }
+    }
+    
+
+    render2DProjection(triangle, gridIndices, gSize) {
+        let gStartY = parseInt(gridIndices[0][0] / gSize);
+        let gStartZ = parseInt(gridIndices[0][1] / gSize);
+        let gEndY = Math.ceil(gridIndices[1][0] / gSize);
+        let gEndZ = Math.ceil(gridIndices[1][1] / gSize);
+        gEndY = gEndY === parseInt(gridIndices[1][0] / gSize) ? gEndY + 1 : gEndY;
+        gEndZ = gEndZ === parseInt(gridIndices[1][1] / gSize) ? gEndZ + 1 : gEndZ;
+
+        let gridWidth = gEndZ - gStartZ + 1;
+        let gridHeight = gEndY - gStartY + 1;
+
+        let gridMap = new Array(gridHeight).fill(0).map(() => new Array(gridWidth).fill(0));
+
+        let x0 = triangle[0][0];
+        let y0 = triangle[0][1];
+        let x1 = triangle[1][0];
+        let y1 = triangle[1][1];
+        let x2 = triangle[2][0];
+        let y2 = triangle[2][1];
+
+        this.drawLine(gridMap, x0, y0, x1, y1, gSize);
+        this.drawLine(gridMap, x0, y0, x2, y2, gSize);
+        this.drawLine(gridMap, x1, y1, x2, y2, gSize);
+
+        console.log(gridMap);
+
+        for (let ii = 0; ii < gridMap.length; ++ii) {
+            for (let jj = 0; jj < gridMap[0].length; ++jj) {
+                if (gridMap[ii][jj] === 1) {
+                    this.renderAuxiliaryObjects(false, false, true, gSize, 0x0000ff, [
+                        0, ii, jj,
+                        1, ii + 1, jj + 1
+                    ]);
+                } else if (gridMap[ii][jj] === 2) {
+                    this.renderAuxiliaryObjects(false, false, true, gSize, 0xff00ff, [
+                        0, ii, jj,
+                        1, ii + 1, jj + 1
+                    ]);
+                }
+            }
+
+        }
+
+    }
+
     testTriangleVoxel() {
         console.log("This is just a test =============================");
         const triangle = new THREE.Triangle();
-        const offSet = 20;
+        const offSet = 36;
 
         // X projection - Right
-        triangle.a.set(10, 0, 30);
-        triangle.b.set(0, 0, 0);
-        triangle.c.set(10, 40,  30);
-
         // triangle.a.set(10, 0, 30);
         // triangle.b.set(0, 0, 0);
-        // triangle.c.set(10, 1,  30);
+        // triangle.c.set(10, 40,  30);
+
+        triangle.a.set(10, 15, 30);
+        triangle.b.set(0, 0, 0);
+        triangle.c.set(10, 40, 15);
 
         // Y projection - Top
         // triangle.a.set(20, 20, 40);
@@ -124,9 +303,9 @@ class IntersectionTest {
         // triangle.c.set(40, 0,  0);
 
         // Z projection - Front
-        // triangle.a.set(-16 - basePt.x + offSet, 11  - basePt.y + offSet, 17  - basePt.z + offSet);
-        // triangle.b.set(-32  - basePt.x + offSet, 32  - basePt.y + offSet, -8 - basePt.z + offSet);
-        // triangle.c.set(-34  - basePt.x + offSet, 13  - basePt.y + offSet,  - basePt.z + offSet);
+        // triangle.a.set(-16  + offSet, 11  + offSet, 17  + offSet);
+        // triangle.b.set(-32  + offSet, 32 + offSet, -8  + offSet);
+        // triangle.c.set(-34  + offSet, 13  + offSet,  offSet);
 
         // Z projection 2 - Front
         // triangle.a.set(20, 40, 20);
@@ -144,7 +323,21 @@ class IntersectionTest {
 
         this.renderProcessedTriangles(triangle);
 
-        this.renderAuxiliaryObjects(true, false, false, gridSize);
+        let _gridSize = gridSize;
+
+        this.render2DProjection(
+            [
+                [triangle.a.y, triangle.a.z],
+                [triangle.b.y, triangle.b.z],
+                [triangle.c.y, triangle.c.z]
+            ],
+            [
+                [minY, minZ],
+                [maxY, maxZ]
+            ],
+            _gridSize
+        );
+
     }
 
     renderProcessedTriangles(triangle) {
@@ -173,6 +366,7 @@ class IntersectionTest {
         shouldRenderBoundary = false,
         shouldRenderGrids = false,
         sceneGridSize,
+        newColor = 0x0000ff,
         gridBoundary = [0, 0, 0, 32, 32, 32]) {
         if (shouldRenderBasePoint) {
             let startingPoint = new THREE.Mesh(
@@ -228,68 +422,11 @@ class IntersectionTest {
             let totalNumberOfGrids2DXY = totalNumberOfGridsX * (endGridY - startGridY);
             let gridGeometry = new THREE.BoxGeometry(sceneGridSize, sceneGridSize, sceneGridSize);
 
-            const newMaterial = new THREE.ShaderMaterial( {
-
-                uniforms: { 
-                    'thickness': { value: 0.3 }, 
-                    'uTime': { value: 0.0 },
-                },
-                vertexShader: `
-                attribute vec3 center;
-                varying vec3 vCenter;
-    
-                flat varying float distanceMask;
-    
-                uniform float uTime;
-    
-                void main() {
-    
-                    vCenter = center;
-    
-                    float dx = position.x;
-                    float dy = position.y;
-                    float freq = sqrt(dx*dx + dy*dy);
-                    distanceMask = max(1.0 - smoothstep(0.0, 65.0, freq), 0.0);
-                    float currentPositionY = 3.0 * sin(0.25 * (uTime - freq)) 
-                        + cos(0.9 * (uTime - freq) + position.y - position.x);
-    
-                    vec3 newPosition = vec3(position.x, position.y, position.z + currentPositionY);
-                    gl_Position = projectionMatrix * modelViewMatrix * vec4( newPosition, 1.0 );
-    
-                }
-                `,
-                fragmentShader: `
-                uniform float thickness;
-    
-                varying vec3 vCenter;
-                flat varying float distanceMask;
-    
-                void main() {
-    
-                    vec3 afwidth = fwidth( vCenter.xyz );
-    
-                    vec3 edge3 = smoothstep( ( thickness - 1.0 ) * afwidth, thickness * afwidth, vCenter.xyz );
-    
-                    float edge = 1.0 - min( min( edge3.x, edge3.y ), edge3.z );
-    
-                    gl_FragColor.rgb = gl_FrontFacing ? vec3( 0.145, 0.589, 0.745 ) : vec3( 0.114, 0.278, 0.341 );
-                    gl_FragColor.a = edge * distanceMask * 0.4;
-    
-                }
-                `,
-                side: THREE.DoubleSide,
-                transparent: true,
-                wireframe: true,
-                alphaToCoverage: true // only works when WebGLRenderer's "antialias" is set to "true"
-    
-            } );
-            newMaterial.extensions.derivatives = true;
-
             let instancedMesh = new THREE.InstancedMesh(
                 gridGeometry,
                 new THREE.MeshBasicMaterial(
                     {
-                        color: 0x0000ff,
+                        color: newColor,
                         wireframe: true
                     }
                 ),
