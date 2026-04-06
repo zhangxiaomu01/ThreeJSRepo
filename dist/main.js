7a031d07f8e439ccaf4f10bfed9b58fc9fce576b
Jun Zhang <31782250+zhangxiaomu01@users.noreply.github.com>
2026-04-06 23:19:31 +0800

    Fixes the shader issue.

--- dist/main.js
+++ dist/main.js
@@ -387,18 +387,23 @@ __webpack_require__.r(__webpack_exports__);
 
 
 const vertexShader = `
+    #define PHONG
+    
     varying vec3 vViewPosition;
-    varying vec3 vNormal;
+    varying vec3 vWorldPosition;
     
     #include <common>
     #include <uv_pars_vertex>
     #include <displacementmap_pars_vertex>
     #include <envmap_pars_vertex>
     #include <color_pars_vertex>
+    #include <fog_pars_vertex>
+    #include <normal_pars_vertex>
     #include <morphtarget_pars_vertex>
     #include <skinning_pars_vertex>
     #include <shadowmap_pars_vertex>
     #include <logdepthbuf_pars_vertex>
+    #include <clipping_planes_pars_vertex>
     
     void main() {
         #include <uv_vertex>
@@ -410,8 +415,7 @@ const vertexShader = `
         #include <skinbase_vertex>
         #include <skinnormal_vertex>
         #include <defaultnormal_vertex>
-        
-        vNormal = normalize(transformedNormal);
+        #include <normal_vertex>
         
         #include <begin_vertex>
         #include <morphtarget_vertex>
@@ -419,16 +423,23 @@ const vertexShader = `
         #include <displacementmap_vertex>
         #include <project_vertex>
         #include <logdepthbuf_vertex>
+        #include <clipping_planes_vertex>
         
         vViewPosition = -mvPosition.xyz;
         
         #include <worldpos_vertex>
+        
+        vWorldPosition = (modelMatrix * vec4(transformed, 1.0)).xyz;
+        
         #include <envmap_vertex>
         #include <shadowmap_vertex>
+        #include <fog_vertex>
     }
 `;
 
 const fragmentShader = `
+    #define PHONG
+    
     uniform vec3 diffuse;
     uniform vec3 emissive;
     uniform vec3 specular;
@@ -442,15 +453,13 @@ const fragmentShader = `
     uniform int numObjectClipPlanes;
     uniform bool clipEnabled;
     
-    varying vec3 vViewPosition;
-    varying vec3 vNormal;
+    varying vec3 vWorldPosition;
     
     #include <common>
     #include <packing>
     #include <dithering_pars_fragment>
     #include <color_pars_fragment>
     #include <uv_pars_fragment>
-    #include <uv2_pars_fragment>
     #include <map_pars_fragment>
     #include <alphamap_pars_fragment>
     #include <alphatest_pars_fragment>
@@ -459,16 +468,17 @@ const fragmentShader = `
     #include <emissivemap_pars_fragment>
     #include <envmap_common_pars_fragment>
     #include <envmap_pars_fragment>
-    #include <cube_uv_reflection_fragment>
     #include <fog_pars_fragment>
     #include <bsdfs>
     #include <lights_pars_begin>
+    #include <normal_pars_fragment>
     #include <lights_phong_pars_fragment>
     #include <shadowmap_pars_fragment>
     #include <bumpmap_pars_fragment>
     #include <normalmap_pars_fragment>
     #include <specularmap_pars_fragment>
     #include <logdepthbuf_pars_fragment>
+    #include <clipping_planes_pars_fragment>
     
     bool isClipped(vec3 worldPosition) {
         if (!clipEnabled) return false;
@@ -498,12 +508,11 @@ const fragmentShader = `
             discard;
         }
         
-        #include <logdepthbuf_fragment>
-        
         vec4 diffuseColor = vec4(diffuse, opacity);
         ReflectedLight reflectedLight = ReflectedLight(vec3(0.0), vec3(0.0), vec3(0.0), vec3(0.0));
         vec3 totalEmissiveRadiance = emissive;
         
+        #include <logdepthbuf_fragment>
         #include <map_fragment>
         #include <color_fragment>
         #include <alphamap_fragment>
@@ -518,14 +527,14 @@ const fragmentShader = `
         #include <lights_fragment_maps>
         #include <lights_fragment_end>
         
+        #include <aomap_fragment>
+        
         vec3 outgoingLight = reflectedLight.directDiffuse + reflectedLight.indirectDiffuse + reflectedLight.directSpecular + reflectedLight.indirectSpecular + totalEmissiveRadiance;
         
         #include <envmap_fragment>
-        
-        gl_FragColor = vec4(outgoingLight, diffuseColor.a);
-        
+        #include <opaque_fragment>
         #include <tonemapping_fragment>
-        #include <encodings_fragment>
+        #include <colorspace_fragment>
         #include <fog_fragment>
         #include <premultiplied_alpha_fragment>
         #include <dithering_fragment>
